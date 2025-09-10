7e99ea667237155c45b015446178fc06d0d2b8d4
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-10 08:34:48 -0300

    prompt-sync lib installed. Also a new exercise that converts celsius temperatures to kelvin.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello, world!")
+const prompt = require('prompt-sync')()
+
+const tempEmCelsius = Number(prompt("Digite uma temperatura em Celsius: "))
+
+const tempEmKelvin = celsiusParaKelvin(tempEmCelsius)
+
+console.log(`A temperatura de ${tempEmCelsius.toFixed(2)}°C é equivalente a ${tempEmKelvin.toFixed(2)}K`)
+
+function celsiusParaKelvin(tempEmCelsius)
+{
+    return tempEmCelsius + 273
+}
